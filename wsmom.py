0a3da728dbb3ca2835e1ed175561811bc9f9c58c
hullrt <59178390+hullrt@users.noreply.github.com>
2023-09-22 18:17:18 -0400

    Fix required folders not being present, and add an iso file name list and remove place iso folder

--- wsmom.py
+++ wsmom.py
@@ -6,15 +6,28 @@ except ModuleNotFoundError:
 import os #Library for OS functions
 import subprocess #Library for running commands
 
+pwd = os.getcwd() #Get the present working directory
+
+#Create needed folders if they don't exist, I can probably simplify this later, but it works for now
+for required_folders in ["usb_files", ".the_curtain"]:
+    if not os.path.exists(os.path.join(pwd, required_folders)):
+        os.makedirs(os.path.join(pwd, required_folders))
+usb_files = f"{pwd}/usb_files"
+
+#List what ISO files are in pwd and ask user to select one
+print("\nISO images found:")
+for iso_file in os.listdir(os.path.join(pwd)):
+    if iso_file.endswith(".iso"):
+        print(iso_file)
+print("\n")
+#Ask user to select an iso file
+path_to_iso = os.path.join(pwd, input("Enter the name of the ISO file you want to use: \n"))
+
 iso = pycdlib.PyCdlib() #Create the PyCdlib object
-pwd = os.getcwd() #Get the current working directory
-path_to_iso = f"{pwd}/Win11_22H2_English_x64v2.iso" #Hardcoded for now
-usb_files = f"{pwd}/usb_files" #Requires folder to exist, currently
-
 iso.open(path_to_iso) #Open the ISO file
-print(path_to_iso) #A lil feedback to terminal
+print(f"\nContinuing with: {path_to_iso}") #A lil feedback to terminal
 
-#walk the iso and get the files and directories
+#Walk the iso and get the files and directories
 for path, dirlist, files in iso.walk(udf_path = '/'): 
     
     #Create the directories
@@ -27,12 +40,13 @@ for path, dirlist, files in iso.walk(udf_path = '/'):
     for file in files:
         full_path = os.path.join(path, file)
         if file.endswith("install.wim"): #passes the cmd to use wimlib on the .wim files
-            print(f"{path_to_iso}{full_path}") #A lil feedback to terminal
-            temp_wim_path = os.path.join(pwd, "the_curtain", "temp_install.wim")
+            
+            print(f"Reticulating splines: {path_to_iso}{full_path}") #A lil feedback to terminal
+            temp_wim_path = os.path.join(pwd, ".the_curtain", "temp_install.wim")
             iso.get_file_from_iso(local_path = temp_wim_path, udf_path=full_path)
-
-            output_swm_path = os.path.join(usb_files, "sources", "install.swm")
+            
             #Splits install.wim into 3800MB chunks and moves to usb_files/sources
+            output_swm_path = os.path.join(usb_files, "sources", "install.swm")
             command = ['wimlib-imagex', 'split', temp_wim_path, output_swm_path, str(3800)]
             subprocess.run(command, capture_output=True, text=True)
             os.remove(temp_wim_path)
